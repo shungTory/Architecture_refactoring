8e224779a211fffc89228153bd6360f0e80c5ab9
pce913 <pce00913@naver.com>
2022-11-23 15:31:49 +0900

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,12 @@
+package evaluation1;
+
+public class Controller {
+    // public void clearRentals()
+    // public void returnVideo()
+    // public void listVideos()
+    // public void listCustomers()
+    // public void getCustomerReport()
+    // public void rentVideo()
+    // public void register(String object) // Customer register 와 video register 혼재
+
+}
